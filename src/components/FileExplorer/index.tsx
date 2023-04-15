9fb9e39bd32e311db2a19f3ccde70e6da694a61e
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2023-04-15 20:37:59 -0300

    fix: remove tag "pre"

--- src/components/FileExplorer/index.tsx
+++ src/components/FileExplorer/index.tsx
@@ -1,7 +1,5 @@
 export async function FileExplorer() {
   return (
-    <div className="w-80 h-content-height border-r-transparent shadow-zinc-200 shadow-xl">
-      <pre>{JSON.stringify(process.env)}</pre>
-    </div>
+    <div className="w-80 h-content-height border-r-transparent shadow-zinc-200 shadow-xl"></div>
   );
 }
