d6d9ad2f3ec46ad2730bbaf2620c27d79f55ec38
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2023-04-23 12:17:42 -0300

    fix: linking signin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
           View my Portfolio
         </Link>
         <Link
-          href="/portfolio"
+          href="/signin"
           className="w-full py-4 bg-zinc-900 text-center border-2 border-solid border-zinc-300 shadow-sm rounded-lg hover:brightness-110 hover:shadow-zinc-400 hover:shadow-md hover:-translate-y-4 hover:scale-105 duration-500 ease-in-out"
         >
           Sign In
